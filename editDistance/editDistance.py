89ce1ec7292cebbb3704d27eecc4751fbca6b185
Goma <gbrmachado@gmail.com>
2017-02-13 19:05:40 -0200

    edit_distanc

--- editDistance/editDistance.py
+++ editDistance/editDistance.py
@@ -1,6 +1,5 @@
 # A Dynamic Programming based Python program for edit
 # distance problem
-
 def edit_distance(str1, str2, m, n):
     """ It calculates the distance """
     # Create a table to store results of subproblems
@@ -13,29 +12,29 @@ def edit_distance(str1, str2, m, n):
             # insert all characters of second string
             if i == 0:
                 dp[i][j] = j    # Min. operations = j
-
+ 
             # If second string is empty, only option is to
             # remove all characters of second string
             elif j == 0:
                 dp[i][j] = i    # Min. operations = i
-
+ 
             # If last characters are same, ignore last char
             # and recur for remaining string
             elif str1[i-1] == str2[j-1]:
                 dp[i][j] = dp[i-1][j-1]
-
+ 
             # If last character are different, consider all
             # possibilities and find minimum
             else:
                 dp[i][j] = 1 + min(dp[i][j-1],        # Insert
                                    dp[i-1][j],        # Remove
                                    dp[i-1][j-1])      # Replace
-
-    return dp[m][n]
+ 
+    return dp
 
 s1 = "snowy"
 s2 = "sunny"
-print edit_distance(s1, s2, len(s1), len(s2)) == 3
+print edit_distance(s1, s2, len(s1), len(s2))
 v1 = "polynomial"
 v2 = "exponential"
-print edit_distance(v1, v2, len(v1), len(v2)) == 6
+print edit_distance(v1, v2, len(v1), len(v2))
